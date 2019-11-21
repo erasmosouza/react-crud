9717d1611cbc5918c369e984fa4f80a740f767a0
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-21 17:56:31 -0300

    The reducer was created for post and the store was configurated in index.js to be acessible in all components

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 
 class PostForm extends Component {
-render() {
-return (
-<div>
-  <h1>Create Post</h1>
-  <form>
-   <input required type="text" placeholder="Enter Post Title" /><br /><br />
-   <textarea required rows="5" cols="28" placeholder="Enter Post" /><br /><br />
-   <button>Post</button>
-  </form>
-</div>
-);
-}
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const title = this.getTitle.value;
+    const message =  this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title,
+      message
+    }
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create Post</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input required type="text" ref={(input)=>this.getTitle = input} 
+          placeholder="Enter Post Title"/>
+          <br /><br />
+          <textarea required rows="5" ref={(input)=>this.getMessage = input} cols="28" 
+          placeholder="Enter Post" />
+          <br /><br />
+          <button>Post</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default PostForm;
